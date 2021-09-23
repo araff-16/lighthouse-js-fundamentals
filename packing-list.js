c0dee1d0a2b690087d2ccd950b203b2711756d4a
araff-16 <alex_raffa16@hotmail.com>
2021-09-23 13:21:00 +0000

    more files added

--- packing-list.js
+++ packing-list.js
@@ -4,3 +4,10 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+console.log("Kitchen stuff to pack:");
+let q = 0
+while (q < packingList.length){
+  console.log(packingList[q]);
+  q++;
+}
